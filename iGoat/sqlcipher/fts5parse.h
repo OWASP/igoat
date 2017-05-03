7ffed7442c8429a8e97cd7b813c165e4a283b7d0
Anthony G <anthony.gonsalves@philips.com>
2017-05-03 13:36:40 +0530

    Added new version of SQL cipher library

---
+++ iGoat/sqlcipher/fts5parse.h
@@ -0,0 +1,14 @@
+#define FTS5_OR                               1
+#define FTS5_AND                              2
+#define FTS5_NOT                              3
+#define FTS5_TERM                             4
+#define FTS5_COLON                            5
+#define FTS5_LP                               6
+#define FTS5_RP                               7
+#define FTS5_MINUS                            8
+#define FTS5_LCP                              9
+#define FTS5_RCP                             10
+#define FTS5_STRING                          11
+#define FTS5_COMMA                           12
+#define FTS5_PLUS                            13
+#define FTS5_STAR                            14
