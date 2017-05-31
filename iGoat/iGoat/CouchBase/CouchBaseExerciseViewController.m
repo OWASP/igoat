b7e75c84a72aed1e5bfd8f3bf4d310ff8aff9aa7
Anthony G <anthony.gonsalves@philips.com>
2017-05-31 21:30:05 +0530

    Added couch base exercise

---
+++ iGoat/iGoat/CouchBase/CouchBaseExerciseViewController.m
@@ -0,0 +1,77 @@
+
+#import "CouchBaseExerciseViewController.h"
+#import "UIAlertController+EasyBlock.h"
+#import <CouchbaseLite/CouchbaseLite.h>
+
+static NSString *const CBKeyPatientName = @"YapKeyPatientName";
+static NSString *const CBKeyPatientAge = @"YapKeyPatientAge";
+static NSString *const CBKeyPatientGender = @"YapKeyPatientGender";
+static NSString *const CBKeyPatientDisease = @"YapKeyPatientDisease";
+
+static NSString *const CBValuePatientName = @"Jane Roe";
+static NSString *const CBValuePatientAge = @"52";
+static NSString *const CBValuePatientGender = @"Female";
+static NSString *const CBValuePatientDisease = @"Cancer";
+
+#define NON_NIL_STRING(str) ((str == nil) ? @"" : str)
+
+@interface CouchBaseExerciseViewController ()
+
+@end
+
+@implementation CouchBaseExerciseViewController {
+    CBLDatabase *database;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    NSError *error = nil;
+    database = [[CBLManager sharedInstance] databaseNamed:@"couchbasedb"
+                                                                 error:&error];
+    [self saveData];
+}
+
+-(IBAction)verifyItemPressed:(id)sender {
+    NSDictionary *info = @{CBKeyPatientName: NON_NIL_STRING(self.nameTextField.text),
+                           CBKeyPatientAge: NON_NIL_STRING(self.ageTextField.text),
+                           CBKeyPatientGender:NON_NIL_STRING(self.genderTextField.text),
+                           CBKeyPatientDisease:NON_NIL_STRING(self.diseaseTextField.text)
+                           };
+    BOOL isVerified = [self verifyPatientInfo:info];
+    NSString *message = (isVerified) ?  @"Success!!" : @"Failed";
+    [UIAlertController showWithTitle:@"iGoat" message:message preferedStyle:UIAlertControllerStyleAlert cancelButtonTitle:@"OK" otherButtonTitles:nil tapBlock:^(UIAlertAction * _Nonnull action, NSInteger buttonIndex) {
+        
+    }];
+}
+
+-(void)saveData {
+    if ([[NSUserDefaults standardUserDefaults]objectForKey:@"documentID"] == nil) {
+        NSDictionary *properties = @{
+                                     CBKeyPatientName : CBValuePatientName,
+                                     CBKeyPatientAge : CBValuePatientAge,
+                                     CBKeyPatientGender : CBValuePatientGender,
+                                     CBKeyPatientDisease : CBValuePatientDisease
+                                     };
+        CBLDocument *newDocument = [database createDocument];
+        [newDocument putProperties:properties error:nil];
+        [[NSUserDefaults standardUserDefaults] setObject:newDocument.documentID forKey:@"documentID"];
+    }
+}
+
+-(BOOL)verifyPatientInfo:(NSDictionary *)info {
+    NSString *documentID = [[NSUserDefaults standardUserDefaults]objectForKey:@"documentID"];
+    if (documentID) {
+        CBLDocument *document = [database documentWithID:documentID];
+        return
+        ([document propertyForKey:CBKeyPatientName] == info[CBKeyPatientName] &&
+         [document propertyForKey:CBKeyPatientAge] == info[CBKeyPatientAge] &&
+         [document propertyForKey:CBKeyPatientGender] == info[CBKeyPatientGender] &&
+         [document propertyForKey:CBKeyPatientDisease] == info[CBKeyPatientDisease]) ? true : false;
+         
+    }
+    return false;
+}
+
+
+@end
