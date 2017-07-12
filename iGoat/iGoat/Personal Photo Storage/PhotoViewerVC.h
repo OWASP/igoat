9eab590883461d3384ac6a752e4a29c3185695f8
Junard Lebajan <junard.lebajan@gemalto.com>
2017-07-12 22:25:21 +0800

    Added Personal photo storage exercise.

---
+++ iGoat/iGoat/Personal Photo Storage/PhotoViewerVC.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewerVC.h
+//  iGoat
+//
+//  Created by Swaroop Yermalkar on 20/06/17.
+//  Copyright © 2017 KRvW Associates, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewerVC : UIViewController
+
+@end
