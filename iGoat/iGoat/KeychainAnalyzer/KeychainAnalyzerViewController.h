92b48a8dc6db99b980f7e315bbc9d4691af6b0ac
heefan <heefan@gmail.com>
2017-07-30 22:50:43 +0800

    add keychain analyzer UI

---
+++ iGoat/iGoat/KeychainAnalyzer/KeychainAnalyzerViewController.h
@@ -0,0 +1,15 @@
+//
+//  KeychainAnalyzerViewController.h
+//  iGoat
+//
+//  Created by heefan on 30/7/17.
+//  Copyright © 2017 KRvW Associates, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ExerciseViewController.h"
+
+
+@interface KeychainAnalyzerViewController : ExerciseViewController
+
+@end
---
+++ iGoat/iGoat/KeychainAnalyzer/KeychainAnalyzerViewController.m
@@ -0,0 +1,38 @@
+//
+//  KeychainAnalyzerViewController.m
+//  iGoat
+//
+//  Created by heefan on 30/7/17.
+//  Copyright © 2017 KRvW Associates, LLC. All rights reserved.
+//
+
+#import "KeychainAnalyzerViewController.h"
+
+@interface KeychainAnalyzerViewController ()
+
+@end
+
+@implementation KeychainAnalyzerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
