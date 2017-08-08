3146521d9e1aa4bb85a28e8046ead119e7dbd32e
Anthony G <gp.anthony@gmail.com>
2017-08-08 20:50:02 +0530

    added social engineering show password

--- iGoat/iGoat/Key Storage Server Side/KeyStorageServerSideVC.m
+++ iGoat/iGoat/Key Storage Server Side/KeyStorageServerSideVC.m
@@ -30,9 +30,21 @@ static NSString *const SecretMessage = @"SecretPass";
     // Dispose of any resources that can be recreated.
 }
 
+
+
 -(void) fetchKeyFromServer {
-    NSString *urlString = @"http://ec2-13-59-35-177.us-east-2.compute.amazonaws.com/crypto1/cryptoKey.php?token=key";
-    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]
+    NSString *urlString = @"http://ec2-13-59-35-177.us-east-2.compute.amazonaws.com/crypto1/cryptoKey.php";
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    urlRequest.HTTPBody = [@"token=key" dataUsingEncoding:NSUTF8StringEncoding];
+    urlRequest.HTTPMethod = @"POST";
+
+    
+    NSDictionary *headers = @{ @"content-type": @"application/x-www-form-urlencoded",
+                               @"accept": @"text/html,application/xhtml+xml,application/xml" };
+    
+    [urlRequest setAllHTTPHeaderFields:headers];
+    
+    [NSURLConnection sendAsynchronousRequest: urlRequest
                                        queue:[NSOperationQueue new] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
                                            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
                                            if (!connectionError && [httpResponse statusCode] == 200) {
@@ -48,7 +60,8 @@ static NSString *const SecretMessage = @"SecretPass";
 -(void) encryptUsingResponseString:(NSString *)responseString {
     self.encryptionKey = [self keyFromResponseString:responseString];
     dispatch_async(dispatch_get_main_queue(), ^{
-        NSString *encryptedString = [[NSString alloc] initWithData:[[SecretMessage dataUsingEncoding:NSUTF8StringEncoding] dataUsingAES256EncryptionWithKey:self.encryptionKey] encoding:NSUTF8StringEncoding];
+        NSData *encryptedData = [[SecretMessage dataUsingEncoding:NSUTF8StringEncoding] dataUsingAES256EncryptionWithKey:self.encryptionKey];
+        NSString *encryptedString = [[NSString alloc] initWithData:encryptedData encoding:NSASCIIStringEncoding];
         self.encryptedMessageLabel.text = encryptedString;
     });
 }
