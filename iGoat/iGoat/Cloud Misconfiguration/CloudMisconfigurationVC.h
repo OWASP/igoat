fbbb7129afb93e9c21cea3b390193de84c9c8a57
Swaroop Yermalkar <swaroop.sy@gmail.com>
2017-08-29 17:29:02 +0530

    Bug fix

---
+++ iGoat/iGoat/Cloud Misconfiguration/CloudMisconfigurationVC.h
@@ -0,0 +1,5 @@
+#import "ExerciseViewController.h"
+
+@interface CloudMisconfigurationVC : ExerciseViewController
+
+@end
---
+++ iGoat/iGoat/Memory Management/MemoryManagementVC.h
@@ -0,0 +1,6 @@
+
+#import <UIKit/UIKit.h>
+#import "ExerciseViewController.h"
+
+@interface MemoryManagementVC : ExerciseViewController
+@end
